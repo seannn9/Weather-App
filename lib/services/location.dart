389568c6e79c425151598e2eb0ebaa458f7cd106
seannn9 <naeskirlu9@gmail.com>
2023-12-30 22:46:11 +0800

    initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,16 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double lon = 0, lat = 0;
+  void getLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      lon = position.longitude;
+      lat = position.latitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
